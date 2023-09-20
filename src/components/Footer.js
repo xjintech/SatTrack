6109bd4e262ce00c6503203ba43ba53701d74e9a
xjintech <142853221+xjintech@users.noreply.github.com>
2023-09-20 13:12:14 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2023 StarLink Tracker. All Rights Reserved. Website Made by Allen
+            </footer>
+        );
+    }
+}
+
+export default Footer;
